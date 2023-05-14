ed066af59c231c6ed768c9d3c4150903c0499368
weibill0409 <130767583+weibill0409@users.noreply.github.com>
2023-05-14 10:41:36 +0800

    vector space model.py

--- vector space model.py
+++ vector space model.py
@@ -1,130 +1,107 @@
 import datetime
+import os
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
 
-# load doc list
-with open('doc_list.txt') as f:
-    doc_list = f.read().splitlines()
-
-# load doc from list
-docs = {}
-words = set()
-for doc in tqdm(doc_list):
-    with open('docs/' + doc + '.txt') as f:
-        data = f.read()
-        docs[doc] = data.split()
-#         words = words.union(set(docs[doc]))
-
-# load query list
-with open('query_list.txt') as f:
-    query_list = f.read().splitlines()
-
-# load query from list
-queries = {}
-for query in tqdm(query_list):
-    with open('queries/' + query + '.txt') as f:
-        data = f.read()
-        queries[query] = data.split()
-        words = words.union(set(queries[query]))
+lexicom = set()   #all different word in doc & query
 
 
-# # Dictionary save / load
+doc_path = r"C:\Users\user\dataHW1\documents"
+all_doc = os.listdir(doc_path)
+documentofall = {}
+for filename in all_doc:
+    with open(os.path.join("documents", filename), 'r',encoding="utf-8") as f:
+        text = f.read()
+        documentofall[filename] = text.split()
+        #lexicom = lexicom.union(set(documentofall[filename])) #in order to make the system more efficient
 
-# save words
-with open('min_word_list.txt', 'w') as f:
-    f.write(' '.join(words))
-    
-words = list(words)
+query_path = r"C:\Users\user\dataHW1\queries"
+all_query = os.listdir(query_path)
+queryofall = {}
 
-# load words dict from file
-with open('min_word_list.txt') as f:
-    words = f.read().split()
+for filename in all_query:
+    with open(os.path.join("queries", filename), 'r',encoding="utf-8") as f:
+        text = f.read()
+        queryofall[filename] = text.split()
+        lexicom = lexicom.union(set(queryofall[filename]))
 
+lexicom = list(lexicom)
 
-# # Calculate docment-tf, query-tf, df, idf
+#term frequency of document
+tf_list_of_doc = []
+for item in documentofall.values():
+    tf_of_each_doc = []
+    for voc in lexicom:
+        tf_of_each_doc.append(item.count(voc))
+    tf_list_of_doc.append(tf_of_each_doc)
 
-# term frequency in document
-tf_docs_list = []
+#term frequency of query
+tf_list_of_query = []
+for item in queryofall.values():
+    tf_of_each_query = []
+    for voc in lexicom:
+        tf_of_each_query.append(item.count(voc))
+    tf_list_of_query.append(tf_of_each_query)
 
-for content in tqdm(docs.values()):
-    tf_doc = []
-    for word in words:
-        tf_doc.append(content.count(word))
-    tf_docs_list.append(tf_doc)
-
-tf_docs_npy = np.array(tf_docs_list)
-np.save('min_tf_docs_npy', tf_docs_npy)
-
-
-# document frequency
-df_list = []
-
-for word in tqdm(words):
+#document frequency
+df = []
+for word in lexicom:
     count = 0
-    for content in docs.values():
-        if word in content:
-            count += 1
-    df_list.append(count)
-
-df_npy = np.array(df_list)
-np.save('min_df_npy', df_npy)
-
-
-# term frequency in query
-tf_queries_list = []
-
-for content in tqdm(queries.values()):
-    tf_query = []
-    for word in words:
-        tf_query.append(content.count(word))
-    tf_queries_list.append(tf_query)
-
-tf_queries_npy = np.array(tf_queries_list)
-np.save('min_tf_queries_npy', tf_queries_npy)
-
+    for item in documentofall.values():
+        if word in item:
+            count +=1
+    df.append(count)
 
 # inverse document frequency
 idf = []
-docs_len = len(docs)
+doc_length = len(documentofall)
+for fre in df:
+    idf.append(np.log(doc_length/fre))
 
-for df in tqdm(df_npy):
-    idf.append(np.log(1 + (1 + docs_len) / (1 + df)))
+#tf-idf(doc)
+tf_idf_doc = []
+temp = []
+for termfreq in tf_list_of_doc:
+    temp = [x*y for x,y in zip(termfreq,idf)]
+    tf_idf_doc.append(temp) 
+print(len(tf_idf_doc))
 
-idf_npy = np.array(idf)
-np.save('min_idf_npy', idf_npy)
+#tf-idf(query)
+tf_idf_query = []
+temp = []
+for termfreq in tf_list_of_query:
+    temp = [x*y for x,y in zip(termfreq,idf)]
+    tf_idf_query.append(temp) 
+print(len(tf_idf_query))
 
+#cosine similarity
+cosine_of_doc_query = []
+cosine_of_doc_query = cosine_similarity(tf_idf_doc, tf_idf_query)
 
-# # Load calculated matrix (save calculate time)
+#doc_list
+with open('docs_id_list.txt') as f:
+    docs_id_list = f.read().splitlines()
 
-tf_docs_npy = np.load('min_tf_docs_npy.npy')
-tf_queries_npy = np.load('min_tf_queries_npy.npy')
-df_npy = np.load('min_df_npy.npy')
-idf_npy = np.load('min_idf_npy.npy')
+#query_list
+with open('queries_id_list.txt') as f:
+    query_id_list = f.read().splitlines()
 
-# # Calculate TF-IDF(doc, query)
-# term weight (tf i,j)*log(1 + (1+N)/(1+ni))
+#panda Dataframe for similarity
+twodarray = pd.DataFrame(cosine_of_doc_query)
+twodarray.index = docs_id_list
+twodarray.columns = query_id_list
+print(twodarray)
 
-tf_idf_docs = tf_docs_npy * idf_npy
-tf_idf_queries = tf_queries_npy * idf_npy
-	
-# # cosine similarity (doc, query)
-cosine_npy = cosine_similarity(tf_idf_docs, tf_idf_queries)
-
-# # sort and export result
-sim_df = pd.DataFrame(cosine_npy)
-sim_df.index = doc_list
-sim_df.columns = query_list
-
+#result
 now = datetime.datetime.now()
-save_filename = 'results/result' + '_' + now.strftime("%y%m%d_%H%M") + '.txt'
+save_filename = 'result' + '_' + now.strftime("%y%m%d_%H%M") + '.txt'
 
 print(save_filename)
 
 with open(save_filename, 'w') as f:
     f.write('Query,RetrievedDocuments\n')
-    for query in query_list:
+    for query in query_id_list:
         f.write(query + ",")
-        query_sim_df = sim_df[query].sort_values(ascending=False)
-        f.write(' '.join(query_sim_df.index.to_list()) + '\n')
+        listedscore = twodarray[query].sort_values(ascending=False)
+        f.write(' '.join(listedscore.index.to_list())+"\n")
